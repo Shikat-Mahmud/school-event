4e0947946c93db63b9b88f1f344eb756c3addf28
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-05-15 19:45:22 +0600

    banner image added

--- resources/views/frontend/main/home.blade.php
+++ resources/views/frontend/main/home.blade.php
@@ -2,101 +2,114 @@
 @section('title', 'School Event')
 @section('content')
         <!-- BANNER SECTION START -->
-        <section class="et-2-banner relative bg-etBlue max-w-[1920px]">
-            <div class="et-banner-slider swiper">
-                <div class="swiper-wrapper">
-                    <!-- single slide -->
-                    <div class="swiper-slide">
-                        <div class="bg-no-repeat bg-cover bg-[url('{{asset('/')}}frontend/img/banner-2-bg-1.jpg')] bg-center pt-[clamp(140px,12.7vw,213px)] text-white relative overflow-hidden z-[1] before:content-normal before:absolute before:inset-0 before:bg-gradient-to-r before:from-[#1900B1] before:from-50% before:to-transparent before:-z-[1] before:opacity-50">
-                            <div class="mx-[15.5em] xxxl:mx-[10em] xxl:mx-[40px] xs:mx-[12px] mb-[132px] xl:mb-[112px] lg:mb-[82px]">
-                                <div class="flex md:flex-col items-center justify-between gap-x-[30px] gap-y-[30px] md:grid-cols-1">
-                                    <div class="left relative z-[20] w-[80%] md:w-full">
-                                        <h6 class="font-kalam font-bold text-[2.4rem] mb-[3px] anim-text">Recall Your Best Student Life</h6>
-
-                                        <h1 class="text-[clamp(42px,6.25vw,12rem)] font-semibold leading-[1.1] mb-[36px] md:mb-[36px] anim-text">Ex Students <span class="italic font-normal">Child Care Academy</span></h1>
-
-                                        <div class="et-banner-btns flex flex-wrap items-center gap-[20px]">
-                                            <!-- <a href="#" class="et-btn bg-etBlue inline-flex items-center justify-center gap-x-[13px] h-[45px] px-[15px] text-white font-normal text-[17px] rounded-full hover:!bg-white hover:!text-etBlue">Register Now</a> -->
-
-                                            <a href="#" class="et-btn bg-white inline-flex items-center justify-center gap-x-[13px] h-[45px] px-[15px] text-etBlue font-normal text-[17px] rounded-full">Register Now</a>
-                                            <!-- <a href="#" class="et-btn bg-white inline-flex items-center justify-center gap-x-[13px] h-[45px] px-[15px] text-etBlue font-normal text-[17px] rounded-full">View More</a> -->
-                                        </div>
-                                    </div>
-
+        <section class="et-2-banner relative bg-etBlue max-w-[1920px]" style="background-image: url('{{asset('/')}}frontend/img/banner-2-bg-1.jpg');">
+            <div class="swiper-wrapper">
+                <!-- single slide -->
+                <div
+                    class="bg-no-repeat bg-cover bg-center pt-[clamp(140px,12.7vw,213px)] text-white relative overflow-hidden z-[1] before:content-normal before:absolute before:inset-0 before:bg-gradient-to-r before:from-[#1900B1] before:from-50% before:to-transparent before:-z-[1] before:opacity-50">
+                    <div class="mx-[15.5em] xxxl:mx-[10em] xxl:mx-[40px] xs:mx-[12px] mb-[132px] xl:mb-[112px] lg:mb-[82px]">
+                        <div class="flex md:flex-col items-center justify-between gap-x-[30px] gap-y-[30px] md:grid-cols-1">
+                            <div class="left relative z-[20] w-[80%] md:w-full">
+                                <h6 class="font-kalam font-bold text-[2.4rem] mb-[3px] anim-text">Recall Your Best Student Life
+                                </h6>
+        
+                                <h1
+                                    class="text-[clamp(42px,6.25vw,12rem)] font-semibold leading-[1.1] mb-[36px] md:mb-[36px] anim-text"> Ex Students <span class="italic font-normal">Child Care Academy</span></h1>
+        
+                                <div class="et-banner-btns flex flex-wrap items-center gap-[20px]">
+                                    <!-- <a href="#" class="et-btn bg-etBlue inline-flex items-center justify-center gap-x-[13px] h-[45px] px-[15px] text-white font-normal text-[17px] rounded-full hover:!bg-white hover:!text-etBlue">Register Now</a> -->
+        
+                                    <a href="#"
+                                        class="et-btn bg-white inline-flex items-center justify-center gap-x-[13px] h-[45px] px-[15px] text-etBlue font-normal text-[17px] rounded-full">Register Now</a>
+                                    <!-- <a href="#" class="et-btn bg-white inline-flex items-center justify-center gap-x-[13px] h-[45px] px-[15px] text-etBlue font-normal text-[17px] rounded-full">View More</a> -->
                                 </div>
                             </div>
-
-                            <!-- counter & video button -->
-                            <div class="flex gap-[120px] xxxl:gap-[60px] xl:gap-[40px] lg:flex-col-reverse items-center">
-                                <div class="max-w-[1170px] bg-etBlue relative z-[1] p-[60px] sm:p-[40px] xs:px-[20px] rounded-tr-[20px] lg:rounded-none overflow-hidden shadow-[0_4px_50px_rgba(18,96,254,0.2)] after:absolute after:inset-0 after:-z-[1] after:bg-[url(../assets/img/et-counter-bg.jpg)] after:bg-cover after:bg-no-repeat after:mix-blend-screen flex xxl:flex-col items-center xxl:items-start md:flex-wrap gap-y-[20px]">
-                                    <div class="pr-[54px] border-r md:border-r-0 border-white/20 md:text-center md:pr-0 md:w-full">
-                                        <h2 class="et-section-title !text-white mb-[28px] anim-text">Count Every Second Until Event.</h2>
-                                        <a href="#" class="et-btn bg-etBlue inline-flex items-center justify-center gap-x-[13px] h-[45px] px-[15px] text-white font-normal text-[17px] rounded-full border-white border hover:!bg-white hover:!text-etBlue">Buy Ticket</a>
-                                    </div>
-
-                                    <!-- counter -->
-                                    <div class="et-countdown flex sm:flex-wrap justify-center gap-y-[10px] md:w-full *:border-r *:sm:border-r-0 *:border-white/20 *:px-[46px] *:sm:px-[26px] *:xs:px-[16px] font-medium text-white text-[16px] text-center">
-                                        <div class="last:pr-0 last:md:pr-[30px] last:xs:pr-[20px] last:border-r-0">
-                                            <span class="days number block"></span>
-                                            <span>Days</span>
-                                        </div>
-
-                                        <div class="last:pr-0 last:md:pr-[30px] last:xs:pr-[20px] last:border-r-0">
-                                            <span class="hours number block"></span>
-                                            <span>Hours</span>
-                                        </div>
-
-                                        <div class="last:pr-0 last:md:pr-[30px] last:xs:pr-[20px] last:border-r-0">
-                                            <span class="minutes number block"></span>
-                                            <span>Minutes</span>
-                                        </div>
-
-                                        <div class="last:pr-0 last:md:pr-[30px] last:xs:pr-[20px] last:border-r-0">
-                                            <span class="seconds number block"></span>
-                                            <span>Seconds</span>
-                                        </div>
-                                    </div>
-
-                                    <!-- vectors -->
-                                    <div class="sm:hidden">
-                                        <img src="{{asset('/')}}frontend/img/counter-vector.png" alt="vector" class="pointer-events-none absolute top-[26px] left-[53%]">
-                                        <img src="{{asset('/')}}frontend/img/counter-vector.png" alt="vector" class="pointer-events-none absolute bottom-[33px] right-[90px]">
-                                    </div>
+        
+                        </div>
+                    </div>
+        
+                    <!-- counter & video button -->
+                    <div class="flex gap-[120px] xxxl:gap-[60px] xl:gap-[40px] lg:flex-col-reverse items-center">
+                        <div
+                            class="max-w-[1170px] bg-etBlue relative z-[1] p-[60px] sm:p-[40px] xs:px-[20px] rounded-tr-[20px] lg:rounded-none overflow-hidden shadow-[0_4px_50px_rgba(18,96,254,0.2)] after:absolute after:inset-0 after:-z-[1] after:bg-[url(../assets/img/et-counter-bg.jpg)] after:bg-cover after:bg-no-repeat after:mix-blend-screen flex xxl:flex-col items-center xxl:items-start md:flex-wrap gap-y-[20px]">
+                            <div class="pr-[54px] border-r md:border-r-0 border-white/20 md:text-center md:pr-0 md:w-full">
+                                <h2 class="et-section-title !text-white mb-[28px] anim-text">Count Every Second Until Event.
+                                </h2>
+                                <a href="#"
+                                    class="et-btn bg-etBlue inline-flex items-center justify-center gap-x-[13px] h-[45px] px-[15px] text-white font-normal text-[17px] rounded-full border-white border hover:!bg-white hover:!text-etBlue">Buy
+                                    Ticket</a>
+                            </div>
+        
+                            <!-- counter -->
+                            <div
+                                class="et-countdown flex sm:flex-wrap justify-center gap-y-[10px] md:w-full *:border-r *:sm:border-r-0 *:border-white/20 *:px-[46px] *:sm:px-[26px] *:xs:px-[16px] font-medium text-white text-[16px] text-center">
+                                <div class="last:pr-0 last:md:pr-[30px] last:xs:pr-[20px] last:border-r-0">
+                                    <span class="days number block"></span>
+                                    <span>Days</span>
                                 </div>
-
-                                <div class="flex shrink-0 gap-[30px] items-center">
-                                    <a href="{{ route('home') }}" class="group">
-                                        <svg width="123" height="123" viewBox="0 0 123 123" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M89.3191 57.1702V110.277H122V1H12.7234V33.6809H65.8298L2 97.5106L25.4894 121L89.3191 57.1702Z" stroke-width="2" class="stroke-[url(#paint0_linear_6096_354)] transition duration-[400ms] group-hover:stroke-etBlue " />
-                                            <defs>
-                                                <linearGradient id="paint0_linear_6096_354" x1="241.347" y1="-124.347" x2="5.74999" y2="111.25" gradientUnits="userSpaceOnUse">
-                                                    <stop offset="0" stop-color="#1872FE" />
-                                                    <stop offset="1" stop-color="#1972FE" stop-opacity="0" />
-                                                </linearGradient>
-                                            </defs>
-                                        </svg>
-                                    </a>
-
-                                    <div class="flex items-center gap-[15px] ">
-                                        <!-- video button -->
-                                        <!-- <a href="https://www.youtube.com/watch?v=AQleI8oFqZo&amp;t=1s" data-fslightbox="banner-video-1" class="w-[93px] aspect-square rounded-full border border-white/20 flex justify-center items-center text-etBlue ml-auto md:ml-0 relative z-[1] text-[18px] before:absolute before:w-[70px] before:h-[70px] before:bg-white before:rounded-full before:-z-[1] before:transition before:duration-[400ms] hover:text-white hover:border-etBlue hover:before:bg-etBlue animate-[shadow_2s_ease-in_infinite]"><i class="fa-solid fa-play"></i></a>
-                                        <span class="font-light text-[18px] xxs:hidden">View Promo</span> -->
-                                    </div>
+        
+                                <div class="last:pr-0 last:md:pr-[30px] last:xs:pr-[20px] last:border-r-0">
+                                    <span class="hours number block"></span>
+                                    <span>Hours</span>
+                                </div>
+        
+                                <div class="last:pr-0 last:md:pr-[30px] last:xs:pr-[20px] last:border-r-0">
+                                    <span class="minutes number block"></span>
+                                    <span>Minutes</span>
+                                </div>
+        
+                                <div class="last:pr-0 last:md:pr-[30px] last:xs:pr-[20px] last:border-r-0">
+                                    <span class="seconds number block"></span>
+                                    <span>Seconds</span>
                                 </div>
                             </div>
-
+        
                             <!-- vectors -->
-                            <div class="et-banner-vectors">
-                                <div class="absolute left-[26px] top-[235px]"><img src="{{asset('/')}}frontend/img/team-vector.png" alt="vector" class="w-[50px] !animate-none !rotate-0 md:hidden"></div>
-                                <div class="absolute bottom-[352px] left-[845px]"><img src="{{asset('/')}}frontend/img/banner-vector.png" alt="vector"></div>
-                                <div class="absolute top-[214px] right-[190px]"><img src="{{asset('/')}}frontend/img/banner-vector-2.png" alt="vector" class="w-[21px] h-[21px]"></div>
+                            <div class="sm:hidden">
+                                <img src="{{asset('/')}}frontend/img/counter-vector.png" alt="vector"
+                                    class="pointer-events-none absolute top-[26px] left-[53%]">
+                                <img src="{{asset('/')}}frontend/img/counter-vector.png" alt="vector"
+                                    class="pointer-events-none absolute bottom-[33px] right-[90px]">
+                            </div>
+                        </div>
+        
+                        <div class="flex shrink-0 gap-[30px] items-center">
+                            <a href="{{ route('home') }}" class="group">
+                                <svg width="123" height="123" viewBox="0 0 123 123" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M89.3191 57.1702V110.277H122V1H12.7234V33.6809H65.8298L2 97.5106L25.4894 121L89.3191 57.1702Z"
+                                        stroke-width="2"
+                                        class="stroke-[url(#paint0_linear_6096_354)] transition duration-[400ms] group-hover:stroke-etBlue " />
+                                    <defs>
+                                        <linearGradient id="paint0_linear_6096_354" x1="241.347" y1="-124.347" x2="5.74999"
+                                            y2="111.25" gradientUnits="userSpaceOnUse">
+                                            <stop offset="0" stop-color="#1872FE" />
+                                            <stop offset="1" stop-color="#1972FE" stop-opacity="0" />
+                                        </linearGradient>
+                                    </defs>
+                                </svg>
+                            </a>
+        
+                            <div class="flex items-center gap-[15px] ">
+                                <!-- video button -->
+                                <!-- <a href="https://www.youtube.com/watch?v=AQleI8oFqZo&amp;t=1s" data-fslightbox="banner-video-1" class="w-[93px] aspect-square rounded-full border border-white/20 flex justify-center items-center text-etBlue ml-auto md:ml-0 relative z-[1] text-[18px] before:absolute before:w-[70px] before:h-[70px] before:bg-white before:rounded-full before:-z-[1] before:transition before:duration-[400ms] hover:text-white hover:border-etBlue hover:before:bg-etBlue animate-[shadow_2s_ease-in_infinite]"><i class="fa-solid fa-play"></i></a>
+                                                <span class="font-light text-[18px] xxs:hidden">View Promo</span> -->
                             </div>
                         </div>
                     </div>
-
+        
+                    <!-- vectors -->
+                    <div class="et-banner-vectors">
+                        <div class="absolute left-[26px] top-[235px]"><img src="{{asset('/')}}frontend/img/team-vector.png"
+                                alt="vector" class="w-[50px] !animate-none !rotate-0 md:hidden"></div>
+                        <div class="absolute bottom-[352px] left-[845px]"><img
+                                src="{{asset('/')}}frontend/img/banner-vector.png" alt="vector"></div>
+                        <div class="absolute top-[214px] right-[190px]"><img
+                                src="{{asset('/')}}frontend/img/banner-vector-2.png" alt="vector" class="w-[21px] h-[21px]">
+                        </div>
+                    </div>
                 </div>
-
-                <div class="et-banner-slider-dots absolute flex flex-col right-[40px] xxs:right-[15px] !left-auto z-[1] !top-[50%] -translate-y-[50%] !w-auto"></div>
             </div>
         </section>
         <!-- BANNER SECTION END -->
@@ -171,7 +184,7 @@
 
 
         <!-- EVENT SCHEDULE SECTION START -->
-        <div class="bg-[#00339A] relative z-[1] after:absolute after:inset-0 after:bg-[url({{ asset('/')}}frontend/img/features-bg.jpg)] after:bg-no-repeat after:bg-cover after:-z-[2] after:pointer-events-none after:opacity-50 pb-[130px] xl:pb-[80px] md:pb-[60px] pt-[244px] xl:pt-[194px] md:pt-[174px]">
+        <div class="bg-[#00339A] relative z-[1] after:absolute after:inset-0 after:bg-no-repeat after:bg-cover after:-z-[2] after:pointer-events-none after:opacity-50 pb-[130px] xl:pb-[80px] md:pb-[60px] pt-[244px] xl:pt-[194px] md:pt-[174px]"  style="background-image: url('{{asset('/')}}frontend/img/features-bg.jpg');">
             <div class="container mx-auto max-w-[calc(100%-37.1vw)] xxxl:max-w-[calc(100%-350px)] xl:max-w-[calc(100%-170px)] px-[12px] lg:max-w-full rev-slide-up">
                 <!-- heading -->
                 <div class="text-center mb-[52px]">
