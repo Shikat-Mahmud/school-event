d96cfa9617d5eb708a0889b14b1ee65933be4dbc
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-05-18 02:20:06 +0600

    review form view update

--- resources/views/frontend/main/review.blade.php
+++ resources/views/frontend/main/review.blade.php
@@ -2,9 +2,22 @@
 @section('title', 'Review Form')
 @section('content')
 
+<!-- BREADCRUMB SECTION START -->
+<section class="et-breadcrumb bg-[#000D83] pt-[146px] lg:pt-[146px] sm:pt-[146px] pb-[130px] lg:pb-[110px] sm:pb-[80px] relative z-[1] before:absolute before:inset-0 before:bg-[url('assets/img/breadcrumb-bg.jpg')] before:bg-no-repeat before:bg-cover before:bg-center before:-z-[1] before:opacity-30">
+    <div class="container mx-auto max-w-[1200px] px-[12px] xl:max-w-full text-center text-white">
+        <h1 class="et-breadcrumb-title font-medium text-[56px] md:text-[50px] xs:text-[45px]">Review Event</h1>
+        <ul class="inline-flex items-center gap-[10px] font-medium text-[16px]">
+            <li class="opacity-80"><a href="{{ route('home') }}" class="hover:text-etBlue">Home</a></li>
+            <li><i class="fa-solid fa-angle-right"></i><i class="fa-solid fa-angle-right"></i></li>
+            <li class="current-page">Review</li>
+        </ul>
+    </div>
+</section>
+<!-- BREADCRUMB SECTION END -->
+
 <!-- CONTACT SECTION START -->
-<div class="py-[120px] xl:py-[80px] md:py-[120px]">
-    <div class="container mx-auto max-w-[1200px] px-[12px] xl:max-w-full mt-[19px]">
+<div class="py-[120px] xl:py-[80px] md:py-[60px]">
+    <div class="container mx-auto max-w-[1200px] px-[12px] xl:max-w-full">
         <div class="grid grid-cols-1 justify-items-center gap-[60px] xl:gap-[40px] items-center">
 
             <!-- right side contact form -->
