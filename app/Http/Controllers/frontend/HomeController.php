63ecbad544abcdf421c465061fe1a674fc248761
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-05-20 23:25:20 +0600

    gallery images are showing in the home page

--- app/Http/Controllers/frontend/HomeController.php
+++ app/Http/Controllers/frontend/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Event;
 use App\Models\EventSchedule;
+use App\Models\Gallery;
 use App\Models\Review;
 use Illuminate\Http\Request;
 
@@ -14,6 +15,7 @@ class HomeController extends Controller
         $event = Event::first();
         $event_schedule = EventSchedule::where('event_id', 1)->get();
         $reviews = Review::where('status', 1)->inRandomOrder()->get();
-        return view('frontend.main.home', compact('event', 'event_schedule', 'reviews'));
+        $galleries = Gallery::latest()->limit(8)->get();
+        return view('frontend.main.home', compact('event', 'event_schedule', 'reviews', 'galleries'));
     }
 }
--- resources/views/frontend/main/home.blade.php
+++ resources/views/frontend/main/home.blade.php
@@ -262,85 +262,20 @@
 
 <!-- GALLERY SECTION START -->
 <section class="grid grid-cols-4 lg:grid-cols-3 sm:grid-cols-2">
-    <!-- single gallery item -->
-    <div
-        class="relative z-[1] group before:absolute before:inset-0 before:bg-etBlack/70 before:z-[0] before:opacity-0 before:transition before:duration-[400ms] hover:before:opacity-100">
-        <img src="{{asset('/')}}frontend/img/gallery-img-1.jpg" alt="gallery image">
-        <a href="{{asset('/')}}frontend/img/gallery-img-1.jpg" data-fslightbox="gallery"
-            class="inline-flex items-center justify-center w-[64px] aspect-square rounded-full bg-white text-[25px] absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] opacity-0 group-hover:opacity-100 hover:text-etBlue">
-            <i class="fa-plus fa-regular"></i>
-        </a>
-    </div>
 
+@if (isset($galleries))
+    @foreach ($galleries as $gallery)
     <!-- single gallery item -->
     <div
         class="relative z-[1] group before:absolute before:inset-0 before:bg-etBlack/70 before:z-[0] before:opacity-0 before:transition before:duration-[400ms] hover:before:opacity-100">
-        <img src="{{asset('/')}}frontend/img/gallery-img-2.jpg" alt="gallery image">
-        <a href="{{asset('/')}}frontend/img/gallery-img-2.jpg" data-fslightbox="gallery"
+        <img src="{{asset('storage/' . $gallery->photo)}}" alt="gallery image">
+        <a href="{{asset('storage/' . $gallery->photo)}}" data-fslightbox="gallery"
             class="inline-flex items-center justify-center w-[64px] aspect-square rounded-full bg-white text-[25px] absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] opacity-0 group-hover:opacity-100 hover:text-etBlue">
             <i class="fa-plus fa-regular"></i>
         </a>
     </div>
-
-    <!-- single gallery item -->
-    <div
-        class="relative z-[1] group before:absolute before:inset-0 before:bg-etBlack/70 before:z-[0] before:opacity-0 before:transition before:duration-[400ms] hover:before:opacity-100">
-        <img src="{{asset('/')}}frontend/img/gallery-img-3.jpg" alt="gallery image">
-        <a href="{{asset('/')}}frontend/img/gallery-img-3.jpg" data-fslightbox="gallery"
-            class="inline-flex items-center justify-center w-[64px] aspect-square rounded-full bg-white text-[25px] absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] opacity-0 group-hover:opacity-100 hover:text-etBlue">
-            <i class="fa-plus fa-regular"></i>
-        </a>
-    </div>
-
-    <!-- single gallery item -->
-    <div
-        class="relative z-[1] group before:absolute before:inset-0 before:bg-etBlack/70 before:z-[0] before:opacity-0 before:transition before:duration-[400ms] hover:before:opacity-100">
-        <img src="{{asset('/')}}frontend/img/gallery-img-4.jpg" alt="gallery image">
-        <a href="{{asset('/')}}frontend/img/gallery-img-4.jpg" data-fslightbox="gallery"
-            class="inline-flex items-center justify-center w-[64px] aspect-square rounded-full bg-white text-[25px] absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] opacity-0 group-hover:opacity-100 hover:text-etBlue">
-            <i class="fa-plus fa-regular"></i>
-        </a>
-    </div>
-
-    <!-- single gallery item -->
-    <div
-        class="relative z-[1] group before:absolute before:inset-0 before:bg-etBlack/70 before:z-[0] before:opacity-0 before:transition before:duration-[400ms] hover:before:opacity-100">
-        <img src="{{asset('/')}}frontend/img/gallery-img-5.jpg" alt="gallery image">
-        <a href="{{asset('/')}}frontend/img/gallery-img-5.jpg" data-fslightbox="gallery"
-            class="inline-flex items-center justify-center w-[64px] aspect-square rounded-full bg-white text-[25px] absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] opacity-0 group-hover:opacity-100 hover:text-etBlue">
-            <i class="fa-plus fa-regular"></i>
-        </a>
-    </div>
-
-    <!-- single gallery item -->
-    <div
-        class="relative z-[1] group before:absolute before:inset-0 before:bg-etBlack/70 before:z-[0] before:opacity-0 before:transition before:duration-[400ms] hover:before:opacity-100">
-        <img src="{{asset('/')}}frontend/img/gallery-img-6.jpg" alt="gallery image">
-        <a href="{{asset('/')}}frontend/img/gallery-img-6.jpg" data-fslightbox="gallery"
-            class="inline-flex items-center justify-center w-[64px] aspect-square rounded-full bg-white text-[25px] absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] opacity-0 group-hover:opacity-100 hover:text-etBlue">
-            <i class="fa-plus fa-regular"></i>
-        </a>
-    </div>
-
-    <!-- single gallery item -->
-    <div
-        class="relative z-[1] group before:absolute before:inset-0 before:bg-etBlack/70 before:z-[0] before:opacity-0 before:transition before:duration-[400ms] hover:before:opacity-100">
-        <img src="{{asset('/')}}frontend/img/gallery-img-1.jpg" alt="gallery image">
-        <a href="{{asset('/')}}frontend/img/gallery-img-1.jpg" data-fslightbox="gallery"
-            class="inline-flex items-center justify-center w-[64px] aspect-square rounded-full bg-white text-[25px] absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] opacity-0 group-hover:opacity-100 hover:text-etBlue">
-            <i class="fa-plus fa-regular"></i>
-        </a>
-    </div>
-
-    <!-- single gallery item -->
-    <div
-        class="relative z-[1] group before:absolute before:inset-0 before:bg-etBlack/70 before:z-[0] before:opacity-0 before:transition before:duration-[400ms] hover:before:opacity-100">
-        <img src="{{asset('/')}}frontend/img/gallery-img-7.jpg" alt="gallery image">
-        <a href="{{asset('/')}}frontend/img/gallery-img-7.jpg" data-fslightbox="gallery"
-            class="inline-flex items-center justify-center w-[64px] aspect-square rounded-full bg-white text-[25px] absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] opacity-0 group-hover:opacity-100 hover:text-etBlue">
-            <i class="fa-plus fa-regular"></i>
-        </a>
-    </div>
+    @endforeach
+@endif
 </section>
 <!-- GALLERY SECTION END -->
 
