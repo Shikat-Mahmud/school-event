743fc50bdac9ce6ee30405535180fb66fe1689d3
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-05-20 23:25:51 +0600

    gallery images are showing in the home page

--- resources/views/frontend/main/home.blade.php
+++ resources/views/frontend/main/home.blade.php
@@ -262,20 +262,19 @@
 
 <!-- GALLERY SECTION START -->
 <section class="grid grid-cols-4 lg:grid-cols-3 sm:grid-cols-2">
-
-@if (isset($galleries))
-    @foreach ($galleries as $gallery)
-    <!-- single gallery item -->
-    <div
-        class="relative z-[1] group before:absolute before:inset-0 before:bg-etBlack/70 before:z-[0] before:opacity-0 before:transition before:duration-[400ms] hover:before:opacity-100">
-        <img src="{{asset('storage/' . $gallery->photo)}}" alt="gallery image">
-        <a href="{{asset('storage/' . $gallery->photo)}}" data-fslightbox="gallery"
-            class="inline-flex items-center justify-center w-[64px] aspect-square rounded-full bg-white text-[25px] absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] opacity-0 group-hover:opacity-100 hover:text-etBlue">
-            <i class="fa-plus fa-regular"></i>
-        </a>
-    </div>
-    @endforeach
-@endif
+    @if (isset($galleries))
+        @foreach ($galleries as $gallery)
+        <!-- single gallery item -->
+        <div
+            class="relative z-[1] group before:absolute before:inset-0 before:bg-etBlack/70 before:z-[0] before:opacity-0 before:transition before:duration-[400ms] hover:before:opacity-100">
+            <img src="{{asset('storage/' . $gallery->photo)}}" alt="gallery image">
+            <a href="{{asset('storage/' . $gallery->photo)}}" data-fslightbox="gallery"
+                class="inline-flex items-center justify-center w-[64px] aspect-square rounded-full bg-white text-[25px] absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] opacity-0 group-hover:opacity-100 hover:text-etBlue">
+                <i class="fa-plus fa-regular"></i>
+            </a>
+        </div>
+        @endforeach
+    @endif
 </section>
 <!-- GALLERY SECTION END -->
 
