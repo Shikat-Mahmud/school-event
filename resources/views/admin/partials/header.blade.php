8b1b9d56aa489eed9073963112fa7cb960c11eca
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-06-02 00:30:03 +0600

    admin header updated

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -122,8 +122,10 @@
                                                     @endif
                                                 </div>
                                                 <div class="flex-grow-1 mx-3">
+                                                    @if(auth()->user())
                                                     <h5 class="mb-0">{{ auth()->user()->name }}</h5>
                                                     <span class="link-primary">{{ auth()->user()->email }}</span>
+                                                    @endif
                                                 </div>
                                             </div>
                                         </li>
